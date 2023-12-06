dd15a0737473a89d6c63f79aa22029f3258ec821
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2023-12-06 12:36:41 +0600

    about section added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import styles from "./App.module.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
+	
 function App() {
 	return (
 		<div className={styles.App}>
 			<Navbar />
 			<Hero />
+			<About/>
 		</div>
 	);
 }
