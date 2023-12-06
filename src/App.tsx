2c9b04b9fda6393c8877ac52faee64c4beb50934
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2023-12-06 12:48:22 +0600

    experience section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./App.module.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Experience from "./components/Experience";
 	
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Navbar />
 			<Hero />
 			<About/>
+			<Experience/>
 		</div>
 	);
 }
