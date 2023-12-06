8fc63993437fd56d44b204439eba28905d6941df
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2023-12-06 12:24:28 +0600

    hero section added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import styles from "./App.module.css";
 import Navbar from "./components/Navbar";
-
+import Hero from "./components/Hero";
 function App() {
 	return (
 		<div className={styles.App}>
 			<Navbar />
+			<Hero />
 		</div>
 	);
 }
