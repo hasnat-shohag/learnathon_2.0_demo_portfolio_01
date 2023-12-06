ebc5ec7c2530508fc94ca65e44a1ff27fe4680b3
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2023-12-06 13:36:27 +0600

    project finished

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Experience from "./components/Experience";
-	
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
 function App() {
 	return (
 		<div className={styles.App}>
 			<Navbar />
 			<Hero />
-			<About/>
-			<Experience/>
+			<About />
+			<Experience />
+			<Projects />
+			<Contact />
 		</div>
 	);
 }
---
+++ src/components/Projects/ProjectCard.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styles from "./ProjectCard.module.css";
+import { getImageUrl } from "../../utils";
+
+interface Project {
+	title: string;
+	imageSrc: string;
+	description: string;
+	skills: string[];
+	demo: string;
+	source: string;
+}
+
+interface ProjectCardProps {
+	project: Project;
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({
+	project: { title, imageSrc, description, skills, demo, source },
+}) => {
+	return (
+		<div className={styles.container}>
+			<img
+				src={getImageUrl(imageSrc)}
+				alt={`Image of ${title}`}
+				className={styles.image}
+			/>
+			<h3 className={styles.title}>{title}</h3>
+			<p className={styles.description}>{description}</p>
+			<ul className={styles.skills}>
+				{skills.map((skill, id) => (
+					<li className={styles.skill} key={id}>
+						{skill}
+					</li>
+				))}
+			</ul>
+			<div className="btngroup" style={{justifyContent: "space-between", marginTop: "10px"}}>
+				<a href={demo} className={styles.button}>
+					Demo
+				</a>
+				<a href={source} className={styles.button}>
+					Source
+				</a>
+			</div>
+		</div>
+	);
+};
+export default ProjectCard;
